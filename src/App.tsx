7b2ae44b446bfdee7d51aa2962c35961670f3790
kotaro takahashi <kotaro.t@k4h4shi.com>
2019-10-23 00:32:16 +0900

    imple count incrementer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import firebase from "./firebase";
 
 function App() {
-  const { count, increment, decrement } = useCounter();
-
-  useEffect(() => {
-    increment();
-    return () => {
-      decrement();
-    };
-  }, []);
+  const count = useCountIncrementer();
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>ようこそ!!!</h1>
         <p>
-          現在
           <span className="App-emphasis">{count}人</span>
-          のユーザーがこのページを閲覧しています
+          がここに来ました。
         </p>
+        <p></p>
       </header>
     </div>
   );
@@ -27,20 +21,40 @@ function App() {
 
 export default App;
 
-const useCounter = () => {
-  const [{ count }, setState] = useState({
+const useCountIncrementer = () => {
+  const [state, setState] = useState({
     count: 0
   });
 
-  const increment = () =>
-    setState({
-      count: count + 1
+  const document = firebase
+    .firestore()
+    .collection("pages")
+    .doc("top");
+
+  const increment = async () => {
+    const snapshot = await document.get();
+    const data = snapshot.data();
+    if (data) {
+      await document.set({
+        count: data.count + 1
+      });
+    }
+  };
+
+  useEffect(() => {
+    increment();
+
+    const unsubscribe = document.onSnapshot(doc => {
+      if (doc.exists) {
+        const count: number = doc.get("count");
+        setState({ count });
+      }
     });
 
-  const decrement = () =>
-    setState({
-      count: count - 1
-    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-  return { count, increment, decrement };
+  return state.count;
 };
