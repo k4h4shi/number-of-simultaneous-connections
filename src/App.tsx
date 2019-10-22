79a6a079fc6f6e829f74895a9aa827790955b82e
kotaro takahashi <kotaro.t@k4h4shi.com>
2019-10-22 21:58:00 +0900

    implement views

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const { count, increment, decrement } = useCounter();
+
+  useEffect(() => {
+    increment();
+    return () => {
+      decrement();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ようこそ!!!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          現在
+          <span className="App-emphasis">{count}人</span>
+          のユーザーがこのページを閲覧しています
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const useCounter = () => {
+  const [{ count }, setState] = useState({
+    count: 0
+  });
+
+  const increment = () =>
+    setState({
+      count: count + 1
+    });
+
+  const decrement = () =>
+    setState({
+      count: count - 1
+    });
+
+  return { count, increment, decrement };
+};
