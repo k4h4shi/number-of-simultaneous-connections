dc50eb42f39956a34f273d50417a36e8e77c3713
kotaro takahashi <kotaro.t@k4h4shi.com>
2019-10-23 02:25:49 +0900

    impl simultaneous connections counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import firebase from "./firebase";
+import moment from "moment";
 
 function App() {
-  const count = useCountIncrementer();
+  const LIFE_EXPECTANCY_SEC = 10;
+  const count = useSimultaneousConnectionCounter(LIFE_EXPECTANCY_SEC);
 
   return (
     <div className="App">
@@ -11,7 +13,7 @@ function App() {
         <h1>ようこそ!!!</h1>
         <p>
           <span className="App-emphasis">{count}人</span>
-          がここに来ました。
+          が現在このページにアクセスしています
         </p>
         <p></p>
       </header>
@@ -21,34 +23,49 @@ function App() {
 
 export default App;
 
-const useCountIncrementer = () => {
+const useSimultaneousConnectionCounter = (lifeExpectancySec: number) => {
   const [state, setState] = useState({
     count: 0
   });
 
-  const document = firebase
+  // アクセスログのコレクション
+  const collection = firebase
     .firestore()
     .collection("pages")
-    .doc("top");
+    .doc("top")
+    .collection("accesses");
 
-  const increment = async () => {
-    const snapshot = await document.get();
-    const data = snapshot.data();
-    if (data) {
-      await document.set({
-        count: data.count + 1
-      });
-    }
+  // アクセスログにアクセス履歴を追加する
+  const access = async (at: Date) => {
+    await collection.add({ at });
   };
 
+  // 指定した秒数のスパンでアクセスし続ける
   useEffect(() => {
-    increment();
+    // 最初のアクセス
+    access(new Date());
 
-    const unsubscribe = document.onSnapshot(doc => {
-      if (doc.exists) {
-        const count: number = doc.get("count");
-        setState({ count });
-      }
+    // 最初のアクセス以降は指定した秒数でアクセスし続ける
+    const interval = setInterval(() => {
+      access(new Date());
+    }, 1000 * lifeExpectancySec);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  // 現在時刻から指定した秒数以内にアクセスしたユーザーを取得する
+  useEffect(() => {
+    const unsubscribe = collection.onSnapshot(async snapshot => {
+      const currentDate = moment(new Date());
+      const someSecondsBeforeCurrentDate = currentDate
+        .subtract("seconds", lifeExpectancySec)
+        .toDate();
+
+      const doc = await snapshot.query
+        .where("at", ">", someSecondsBeforeCurrentDate)
+        .get();
+      setState({ ...state, count: doc.size });
     });
 
     return () => {
